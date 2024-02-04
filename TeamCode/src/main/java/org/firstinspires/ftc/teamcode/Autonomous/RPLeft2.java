b57b3dfe4db6700b3c10e5b9f59f8ab261847d4c
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-02-03 19:33:36 -0500

    Disabling Auto's we dont use to clear up Driver Station

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RPLeft2.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import static org.firstinspires.ftc.teamcode.Autonomous.VisionColorDetector.*;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.stream.CameraStreamServer;
+import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.Utilities.Color;
+import org.firstinspires.ftc.teamcode.Utilities.Constants.Constants;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+import org.opencv.core.Rect;
+
+import java.util.Collection;
+
+@Autonomous(name="RP2-Left")
+@Disabled
+
+public class RPLeft2 extends OpMode {
+    VisionColorDetector detector;
+    VisionPortal visionPortal;
+
+    private Drivetrain AsterionMotors;
+    private ElapsedTime RunTime = new ElapsedTime();
+    private Blinker LED;
+    private LynxModule ControlHub;
+    private LynxModule ExpansionHub;
+    private VoltageSensor Battery;
+    private Collection<Blinker.Step> steps;
+
+    private DcMotor AngleMotor;
+    private DcMotor LiftMotor;
+
+    @Override
+    public void init() {
+        AsterionMotors = new Drivetrain(hardwareMap);
+        AsterionMotors.setOpposite(1);
+        AsterionMotors.setBrakingMode("BRAKING");
+
+        ControlHub = Color.getLynxModule(hardwareMap, Constants.ControlHubID);
+        ExpansionHub = Color.getLynxModule(hardwareMap, Constants.ExpansionHubID);
+        Battery = hardwareMap.get(VoltageSensor.class, Constants.ControlHubID);
+        LED = hardwareMap.get(Blinker.class, Constants.ControlHubID);
+        telemetry.addData("Status", "Initialized");
+
+        AngleMotor = hardwareMap.get(DcMotor.class, Constants.AngleMotorID);
+        LiftMotor = hardwareMap.get(DcMotor.class, Constants.LiftMotorID);
+
+        AngleMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+        RunTime.reset();
+
+        ControlHub.setPattern(Color.teleopPattern());
+        ExpansionHub.setPattern(Color.teleopPattern());
+    }
+
+    @Override
+    public void loop() {
+        if (RunTime.seconds() < 2.65) {
+            AsterionMotors.mecanumDrive(-Constants.AutoSpeed, 0, 0);
+        } else if (RunTime.seconds() > 2.65 && RunTime.seconds() < 4) {
+            AsterionMotors.mecanumDrive(0, 0, 0);
+        } else if (RunTime.seconds() > 4 && RunTime.seconds() < 4.9) {
+            AsterionMotors.mecanumDrive(0, 0, 0.3);
+        } else if (RunTime.seconds() > 4.9 && RunTime.seconds() < 5.4) {
+            AsterionMotors.mecanumDrive(0, 0, 0);
+        } else if (RunTime.seconds() > 5.4 && RunTime.seconds() < 5.82) {
+            AsterionMotors.mecanumDrive(-Constants.AutoSpeed, 0, 0);
+        } else if (RunTime.seconds() > 5.82) {
+            AsterionMotors.mecanumDrive(0, 0, 0);
+        }
+    }
+}
