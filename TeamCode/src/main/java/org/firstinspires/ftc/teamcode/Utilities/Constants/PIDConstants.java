56d73880401bc3d8f8e390d1cc8f1f9d43085f4a
ultimate_hecker <86735991+ultimatehecker@users.noreply.github.com>
2024-02-03 19:34:06 -0500

    Trying to get encoders to fix drifting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/Constants/PIDConstants.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Utilities.Constants;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class PIDConstants {
+    public static double Kp = 1.0; // proportional
+    public static double Ki = 0.0; // integral
+    public static double Kd = 0.031; // derivative
+}
